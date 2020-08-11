d30013ebe9355f917f9efd5e4e01dd35343e5136
Madhura Bindu <madhura.bindu@xymob.in>
2020-08-11 21:19:17 +0530

    tele sample

---
+++ TelecomColorCoder/TelecomColorCoder/ColorCoder.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelecomColorCoder
+{
+    class ColorCoder
+    {
+        private static Color[] colorMapMajor;
+        private static Color[] colorMapMinor;
+
+
+        static ColorCoder()
+        {
+            colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet};
+            colorMapMinor = new Color[] {Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
+        }
+
+        public Color[] GetColorFromPairNumber(int pairNumber)
+        {
+            int majorSize = colorMapMajor.Length;
+            int minorSize = colorMapMinor.Length;
+
+            int majorIndex = pairNumber / majorSize;
+            int minorIndex = (pairNumber % minorSize) == 0 ? minorSize-1 : (pairNumber % 5) - 1;
+            return new Color[] { colorMapMajor[majorIndex], colorMapMinor[minorIndex] };
+        }
+
+        public int GetPairNumberFromColor(Color major, Color minor)
+        {
+            int majorIndex = -1;
+            for(int i = 0; i < colorMapMajor.Length; i++)
+            {
+                if(colorMapMajor[i] == major)
+                {
+                    majorIndex = i;
+                    break;
+                }
+            }
+
+            int minorIndex = -1;
+            for (int i = 0; i < colorMapMinor.Length; i++)
+            {
+                if (colorMapMinor[i] == minor)
+                {
+                    minorIndex = i;
+                    break;
+                }
+            }
+
+            if (majorIndex == -1 || minorIndex == -1)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return (majorIndex * colorMapMinor.Length) + (minorIndex + 1);
+        }
+
+        public override string ToString()
+        {
+            string retValue = string.Empty;
+            for (int i = 0; i < colorMapMajor.Length; i++)
+            {
+                for (int j = 0; j < colorMapMinor.Length; j++)
+                {
+                    int pairNumber = GetPairNumberFromColor(colorMapMajor[i], colorMapMinor[j]);
+
+                    retValue = retValue + string.Format("{0} === {1} === {2}\n",pairNumber, colorMapMajor[i].Name, colorMapMinor[j].Name);
+                }
+                retValue = retValue + "\n";
+            }
+
+            return retValue;
+        }
+    }
+}
---
+++ TelecomColorCoder/TelecomColorCoder/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelecomColorCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColorCoder cd = new ColorCoder();
+            Console.WriteLine(  cd.ToString());
+            ColorChecker();
+            PairChecker();
+        }
+
+        static void ColorChecker()
+        { 
+            ColorCoder colorCoder = new ColorCoder();
+            bool loop = true;
+            while (loop)
+            {
+                Console.Write("Enter the pair number between 1-25: ");
+                string input = Console.ReadLine();
+                int pairNumber;
+                if (int.TryParse(input, out pairNumber))
+                {
+                    if (pairNumber > 0 && pairNumber <= 25)
+                    {
+                        var ret = colorCoder.GetColorFromPairNumber(pairNumber);
+                        Console.WriteLine();
+                        Console.WriteLine("{0},{1}", ret[0], ret[1]);
+                    }
+                    else if(pairNumber ==0)
+                    {
+                        loop = false;
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+        }
+
+        static void PairChecker()
+        {
+            ColorCoder colorCoder = new ColorCoder();
+            bool loop = true;
+            while (loop)
+            {
+                Console.Write("Enter the Major Color : ");
+                string input = Console.ReadLine();
+                Color majorColor = Color.FromName(input);
+
+                Console.Write("Enter the Minor Color : ");
+                input = Console.ReadLine();
+                Color minorColor = Color.FromName(input);
+
+                if (majorColor.IsKnownColor && minorColor.IsKnownColor)
+                {
+                    var ret = colorCoder.GetPairNumberFromColor(majorColor, minorColor);
+                    Console.WriteLine();
+                    Console.WriteLine("PairNumber: {0}", ret);
+                }
+                else
+                {
+                    if (majorColor == Color.Pink || minorColor == Color.Pink)
+                    {
+                        loop = false;
+                    }
+                }
+            }
+        }
+    }
+}
